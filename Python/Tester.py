f558192ea2b7930f7b0eef788c737ecc8a71ed7f
Curtis Kortman <curtiso121@gmail.com>
2015-03-19 14:45:10 -0400

    Test to check if i2c is working

--- Python/Tester.py
+++ Python/Tester.py
@@ -1,20 +1,35 @@
 import time
 import datetime
+from Adafruit_I2C.py import *
+
 
 # print('Creating a new text file or deleting the old one')
 # filetest = open('test.txt', 'w')
 # filetest.close()
-a = 1
+# a = 1
 # print(datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M'))
-
+#      
 # while(True):
+#      
+# filetest = open('test.txt', 'a')
+#       
+#    # print(datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M'))
+#    # print ('writing ' +str(a) + '....' + '\n')
+#      
+# filetest.write(' ' + str(a) + ' ')
+# filetest.write(datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M') + '\n')
+# filetest.close()
+# a += 1
 
-filetest = open('test.txt', 'a')
+print(1)
+print (getPiRevision())
 
-    #print(datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M'))
-    #print ('writing ' +str(a) + '....' + '\n')
+print(2)
+print (getPiI2CBusNumber())
 
-filetest.write(' ' + str(a) + ' ')
-filetest.write(datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M') + '\n')
-filetest.close()
-a += 1
+print("done")
+
+
+
+
+
