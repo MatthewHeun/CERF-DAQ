65b3c035068441254fe69d47068528b23dc5e9c1
Curtis Kortman <curtiso121@gmail.com>
2015-03-19 13:41:33 -0400

    First upload test

---
+++ Tester.py
@@ -0,0 +1,20 @@
+import time
+import datetime
+
+# print('Creating a new text file or deleting the old one')
+# filetest = open('test.txt', 'w')
+# filetest.close()
+a = 1
+# print(datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M'))
+
+# while(True):
+
+filetest = open('test.txt', 'a')
+
+    #print(datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M'))
+    #print ('writing ' +str(a) + '....' + '\n')
+
+filetest.write(' ' + str(a) + ' ')
+filetest.write(datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M') + '\n')
+filetest.close()
+a += 1
