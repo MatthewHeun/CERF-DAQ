8a93097856b4363f44f808d5bfc16744a5ba98d3
Derek De Young <derekdeyoung@gmail.com>
2015-04-07 22:08:31 -0400

    created global vars

---
+++ WebPage/pages/global_vars.php
@@ -0,0 +1,11 @@
+<?php
+
+$Pi_Number= "1"; 
+$SENSOR1= "TNT North Wall";
+$SENSOR2= "Sensor 2";
+$SENSOR3= "Sensor 3";
+
+$Summary_Base= "../../../Data/Pi_" . $Pi_Number . "_Summary/";
+$Raw_Base= "../../../Data/Pi_" . $Pi_Number . "_Raw/";
+
+?>
