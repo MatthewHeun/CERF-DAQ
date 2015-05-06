0c685424ad72c60297909f6ed357a82248661cc5
pi1 <cerfpi1@calvin.edu>
2015-05-06 17:16:13 -0400

    updated file structure

--- Scripts/zip_raw.sh
+++ Scripts/zip_raw.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo zip -r /home/pi/Desktop/CERF-DAQ/WebPage/pages/Raw.zip /home/pi/Desktop/Data/Pi_1_Raw/
-sudo zip -r /home/pi/Desktop/CERF-DAQ/WebPage/pages/Summary.zip /home/pi/Desktop/Data/Pi_1_Summary/
+sudo zip -r -j /home/pi/Desktop/CERF-DAQ/WebPage/pages/Raw.zip /home/pi/Desktop/Data/Pi_1_Raw/
+sudo zip -r -j /home/pi/Desktop/CERF-DAQ/WebPage/pages/Summary.zip /home/pi/Desktop/Data/Pi_1_Summary/
 
