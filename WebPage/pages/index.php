e1ba2d6802781036ff43f8462b0d4fe02d405ce8
pi1 <cerfpi1@calvin.edu>
2015-05-13 18:29:06 -0400

    updated link in header

--- WebPage/pages/index.php
+++ WebPage/pages/index.php
@@ -71,7 +71,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">CERF Pi <?php echo $Pi_Number ?></a>
+                <a class="navbar-brand" href="index.php">CERF Pi <?php echo $Pi_Number ?></a>
             </div>
             <!-- /.navbar-header -->
 
