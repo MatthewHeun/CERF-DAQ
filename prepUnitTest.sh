da5382730d507a28d38f2af376334d570f9b9c66
pjh26 <pjh26@PC25877.ad.calvin.edu>
2018-10-24 17:14:36 -0400

    typo

--- prepUnitTest.sh
+++ prepUnitTest.sh
@@ -16,5 +16,5 @@ do
 	unzip -d /home/travis/build/MatthewHeun/Data/Pi_3_Raw/$sensorNum/ /home/travis/build/MatthewHeun/Data/Pi_3_Raw/$sensorNum/$sensorNum.zip
 done
 
-sudo chmod -R 777 /home/travis/build/MatthewHeun/Data/
+chmod -R 777 /home/travis/build/MatthewHeun/Data/
 mkdir /home/travis/build/MatthewHeun/Data/Pi_3_Summary/
