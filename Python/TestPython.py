36b426edb433c71814f4c3029ec761814d2ca11d
pjh26 <pjh26@PC25877.ad.calvin.edu>
2018-11-05 23:18:06 -0500

    test

--- Python/TestPython.py
+++ Python/TestPython.py
@@ -90,7 +90,7 @@ class TestPython(unittest.TestCase):
         summaryFile.close()
         
         def test_getData(self):
-            self.assertEqual(0,0)
+            self.assertEqual(0,1)
         
         def test_piConnectivity(self):
             self.assertEqual(0,0)
