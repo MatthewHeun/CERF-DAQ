89b0141725041e0d098e8bda184ea15801fe147e
pjh26 <pjh26@PC25877.ad.calvin.edu>
2018-11-19 18:14:52 -0500

    Added cron command for connection test, along with test files

--- Scripts/update-pi.sh
+++ Scripts/update-pi.sh
@@ -3,7 +3,7 @@
 echo '0' > "/home/pi/Desktop/CERF-DAQ/WebPage/pages/updateHealth.txt"
 
 yes | sudo apt-get update
-yes | sudo apt-get upgrade
+yes | sudo apt-get dist-upgrade
 
 echo '1' > "/home/pi/Desktop/CERF-DAQ/WebPage/pages/updateHealth.txt"
 
