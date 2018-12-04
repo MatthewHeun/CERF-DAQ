46ade933f594395c4e9321f5879543365a6f56da
Peter Haagsma <pjh26@students.calvin.edu>
2018-12-03 20:21:03 -0500

    Connection test working quite well

--- UnitTest/connectionTest.sh
+++ UnitTest/connectionTest.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-path="/home/pi/Desktop/CERF-DAQ/UnitTest/connectionList.txt"
+connectionList="/home/pi/Desktop/CERF-DAQ/UnitTest/connectionList.txt"
+emailList="/home/pi/Desktop/CERF-DAQ/UnitTest/connectionTestEmails.txt"
 
 while IFS= read -r line
 do
 	if  ! ping -c 1 $line >/dev/null; then
-		swaks --to pjh26@students.calvin.edu -s smtp.gmail.com:587 -tls -au cerfraspberrypi@gmail.com -ap MasterCerf153$ --body "The Pi at IP $line is not responding!!"
+		while read email
+		do
+			swaks --to $email -s smtp.gmail.com:587 -tls -au cerfraspberrypi@gmail.com -ap MasterCerf153$ --body "The Pi at IP $line is not responding!!"
+		done < $emailList
 	else
 		echo "Success!"
 	fi
-done <$path
+done < $connectionList
